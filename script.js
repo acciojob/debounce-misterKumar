cca1133158f030e40a2d3f834fdcb2655d9b86e6
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-18 20:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
+let timeoutId;
+
+  return function(...args) {
+    const context = this;
+
+    const later = function() {
+      timeoutId = null;
+      if (!immediate) {
+        callback.apply(context, args);
+      }
+    };
+
+    const callNow = immediate && !timeoutId;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(later, delay);
+
+    if (callNow) {
+      callback.apply(context, args);
+    }
+  };  
+}
   
   module.exports = debounce;
